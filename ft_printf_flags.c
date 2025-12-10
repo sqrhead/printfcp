265d375cbd5afa497e040bd09dff59dff40e03d2
sqrhead <fshelna98@gmail.com>
2025-12-10 20:40:50 +0100

    momd

--- ft_printf_flags.c
+++ ft_printf_flags.c
@@ -2,24 +2,19 @@
 
 void print_p(void *p, size_t *index)
 {
-	void	*pul;
-
-	pul = va_arg(*args, void *);
 	write(1,"0x",2);
 	*index += 2;
-	print_hex((unsigned long)pul, index);
+	print_hex((unsigned long)p, index);
 }
 
-void print_s(va_list *args, size_t *index)
+void print_s(char *s, size_t *index)
 {
-	char	*str;
 	size_t	i;
 
-	str = va_arg(*args ,char *);
 	i = 0;
-	while (str[i])
+	while (s[i])
 	{
-		write(1, &str[i], 1);
+		write(1, &s[i], 1);
 		i ++;
 	}
 	*index += i;
@@ -32,29 +27,23 @@ void print_c(char c, size_t *index)
 	*index += 1;
 }
 
-void print_di(va_list *args, size_t *index)
+void print_di(int i, size_t *index)
 {
 	unsigned long 	num;
-	int				arg;
 
 	num = 0;
-	arg = va_arg(*args, int);
-	if (!arg)
-		printf("NULL_ARGS\n");
-	if (arg < 0)
+	// arg = va_arg(*args, int);
+	if (i < 0)
 	{
 		write(1,"-",1);
 		*index += 1;
-		arg = arg * -1;
+		i = i * -1;
 	}
-	num = (unsigned long)arg;
+	num = (unsigned long)i;
 	print_dec(num, index);
 }
 
-void print_u(va_list *args, size_t *index)
+void print_u(unsigned int ui, size_t *index)
 {
-	unsigned int	n;
-
-	n = va_arg(*args, unsigned int);
-	print_dec(n, index);
+	print_dec(ui, index);
 }
