f37341dd472302d4f047316a7dcd91d8331930b0
sqrhead <fshelna98@gmail.com>
2025-12-11 23:20:13 +0100

    fuck

--- ft_printf.c
+++ ft_printf.c
@@ -9,7 +9,7 @@ void check(const char *str, size_t *index, va_list args)
 		print_s(va_arg(args, char *), index);
 	else if (str[*index] == 'p')
 		print_p(va_arg(args, void *), index);
-	else if (str[*index] == 'd' || str[*index++] == 'i')
+	else if (str[*index] == 'd' || str[*index] == 'i')
 		print_di(va_arg(args, int), index);
 	else if (str[*index] == 'u')
 		print_u(va_arg(args, unsigned int), index);
@@ -44,45 +44,64 @@ int	ft_printf(const char *format, ...)
 			index ++;
 			s_len ++;
 		}
-		write(1,&format[s_len],1);
-		index ++;
-		s_len ++;
+		else 
+		{
+			write(1,&format[s_len],1);
+			index ++;
+			s_len ++;
+		}
 	}
 
 	va_end(args);
 	return (index);
 }
-
 #include <limits.h>
 int main()
 {
-	char c = 'A';
-	char str[] = "splash";
-	char *strnull = NULL;
-	printf("########### c s ###################### \n");
-	ft_printf("ft_printf { char }  : %c\n",c);
-	ft_printf("ft_print { char*} : %s\n",str);
-	printf("printf { char*} : %s\n",str);
-	printf("########### p ######################### \n");
-	printf("printf { void* } : %p\n",&str);
-	ft_printf("ft_printf { void* }  : %p\n",&str);
-	printf("########### D & I ##################### \n");
-	int num = -130;
-	printf("printf { decimal base 10 } %d\n",num);
-	printf("printf { integer base 10 } %i\n",num);
-	ft_printf("ft_printf { decimal base 10 } %d\n",num);
-	ft_printf("ft_printf { integer base 10 } %i\n",num);
-	printf("########## U ########################## \n");
-	unsigned int un = INT_MIN;
-	printf("printf {unsigned int }  %u\n",un);
-	ft_printf("ft_printf { unsigned int } %u\n",un);
-	printf("######### X x  ######################## \n");
-	int hn = -2030; // 128808997
-	printf("printf { hex lower } %x\n",hn);
-	printf("printf { hex upper } %X\n",hn);
-	ft_printf("ft_printf { hex lower } %x\n",hn);
-	ft_printf("ft_printf { hex upper } %X\n",hn);
-	printf("######## % ########################### \n");
-	printf("printf { percent } %%\n");
-	ft_printf("ft_printf { percent } %%\n");
+	char		*str = "str_test";
+	unsigned int	ui = 20;
+	int		i = -20;
+	int		hex = 120410519;
+	int		len = 0;
+
+	printf("========= FT_PRINTF ==================\n");
+	len =	ft_printf("CHAR		%c\n",str[0]);
+	ft_printf("CHAR LEN		%i\n",i);
+	len =	ft_printf("STR		%s\n",str);
+	ft_printf("STR LEN		%i\n",i);
+	len =	ft_printf("PTR_ADDRESS	%p\n",str);
+	ft_printf("PTR LEN		%i\n",i);
+	len =	ft_printf("INT		%i\n",i);
+	ft_printf("INT LEN		%i\n",i);
+	len =	ft_printf("DEC		%d\n",i);
+	ft_printf("DEC LEN		%i\n",i);
+	len =	ft_printf("UNSIGNED INT	%u\n",ui);
+	ft_printf("UI LEN		%i\n",i);
+	len =	ft_printf("HEX_MIN	%x\n",hex);
+	ft_printf("HEXM LEN		%i\n",i);
+	len =	ft_printf("HEX_UPP	%X\n",hex);
+	ft_printf("HEXU LEN		%i\n",i);
+	len =	ft_printf("PERCENT	%%\n");
+	ft_printf("PER LEN		%i\n",i);
+
+	printf("========= PRINTF ==================\n");
+	len = printf("CHAR		%c\n",str[0]);
+	printf("CHAR LEN		%i\n",i);
+	len = printf("STR		%s\n",str);
+	printf("STR LEN			%i\n",i);
+	len = printf("PTR_ADDRESS	%p\n",str);
+	printf("PTR LEN			%i\n",i);
+	len = printf("INT		%i\n",i);
+	printf("INT LEN			%i\n",i);
+	len = printf("DEC		%d\n",i);
+	printf("DEC LEN			%i\n",i);
+	len = printf("UNSIGNED INT	%u\n",ui);
+	printf("UI LEN			%i\n",i);
+	len = printf("HEX_MIN		%x\n",hex);
+	printf("HEXM LEN		%i\n",i);
+	len = printf("HEX_UPP		%X\n",hex);
+	printf("HEXU LEN		%i\n",i);
+	len = printf("PERCENT		%%\n");
+	printf("PER LEN			%i\n",i);
+	
 }
